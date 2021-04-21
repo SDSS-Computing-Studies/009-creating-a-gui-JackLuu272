be48c7bf50b317a6975d1188c14e39a801282a4b
JackLuu272 <78498034+JackLuu272@users.noreply.github.com>
2021-04-20 23:21:30 -0700

    Program is completed

--- task1.py
+++ task1.py
@@ -5,6 +5,7 @@ from tkinter import ttk
 
 window = tk.Tk()
 window.title("multiplication module")
+window.geometry("307x25")
 
 entryint1 = tk.Entry(window,text = "", width = 10)
 multsign = tk.Label(window,text = "x")
@@ -12,11 +13,11 @@ entryint2 = tk.Entry(window,text = "", width = 10)
 equalsign = tk.Button(window,text = "=" )
 result = tk.Entry(window,text = "", width = 20)
 
-entryint1.grid(row=1, column = 1)
-multsign.grid(row=1, column = 2)
-entryint2.grid(row=1, column = 3)
-equalsign.grid(row=1, column = 4)
-result.grid(row=1, column = 5)
+entryint1.place(x=0, y=0)
+multsign.place(x=70, y=0)
+entryint2.place(x=90, y=0)
+equalsign.place(x=157, y=0)
+result.place(x=180, y=0)
 
 window.mainloop()
 
